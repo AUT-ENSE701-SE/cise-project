50227596c27c491cda31a9c96fba7243d7b989e2
nigelph <54135755+nigelph@users.noreply.github.com>
2020-09-28 22:12:35 +1300

    Create SubmitEvidencePage.js
    Added in Submit evidence webpage form

---
+++ src/components/pages/SubmitEvidencePage.js
@@ -0,0 +1,10 @@
+import React from "react";
+import NavigationBar from "../general-components/NavigationBar.js";
+
+class SubmitEvidencePage extends React.Component {
+  render() {
+    return <NavigationBar />;
+  }
+}
+
+export default SubmitEvidencePage;
